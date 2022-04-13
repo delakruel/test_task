042bbd245e8f9ef8aef334d192b57d45fff38933
rloyce <delakruel@gmail.com>
2022-04-14 00:18:25 +0400

    ready, need to make up

--- main.c
+++ main.c
@@ -17,7 +17,7 @@
 
 typedef struct info
 {
-    unsigned char   rid[10];
+    unsigned char   *rid;
     unsigned char   *pix;
     unsigned short  prior;
 } app_info;
@@ -42,54 +42,6 @@ unsigned short  app_mem(const unsigned char *pRxBuf, unsigned short i, unsigned 
 {
     unsigned short  num = 0;
     unsigned short  l_end;  
-    unsigned short  j;
-
-    while (i < end)
-    {
-        if (pRxBuf[i] == '6' && pRxBuf[i + 1] == '1')//здесь уже в любом случае будет 61, нет смысла проверять, но на всякий
-        {
-            i += 2;
-            l_end = char_to_num(pRxBuf[i], pRxBuf[i + 1]) * 2 + i + 2;
-                // printf("%c", pRxBuf[l_end]);
-            i += 2;
-            while (i < l_end)
-            {
-                if (pRxBuf[i] == '9' && pRxBuf[i + 1] == 'F' && pRxBuf[i + 2] == '2' && pRxBuf[i + 3] == 'A')
-                {
-                    i += 4;
-                    i += 2 + char_to_num(pRxBuf[i], pRxBuf[i + 1]) * 2;
-                }
-                if (pRxBuf[i] == '4' && pRxBuf[i + 1] == 'F')
-                {
-                    i += 2;
-                    j = char_to_num(pRxBuf[i], pRxBuf[i + 1]);
-                    i += 2;
-                    if (pRxBuf[i] == 'A' && pRxBuf[i + 1] == '0' && pRxBuf[i + 2] == '0' && pRxBuf[i + 3] == '0' && pRxBuf[i + 4] == '0' && 
-                       pRxBuf[i + 5] == '0' && pRxBuf[i + 6] == '0' && pRxBuf[i + 7] == '6' && pRxBuf[i + 8] == '5' && pRxBuf[i + 9] == '8')
-                        ++num; // выбираем только поддерживаемые ридером приложения 
-                    i += j * 2;
-                }
-                if (pRxBuf[i] == '8' && pRxBuf[i + 1] == '7')
-                {
-                    i += 2;
-                    j = char_to_num(pRxBuf[i], pRxBuf[i + 1]);
-                    i += j * 2 + 2;
-                }
-            }
-        }
-        else
-            ++i;
-    }
-    return num;
-}
-
-unsigned short  fill_app_info(const unsigned char *pRxBuf, unsigned short i, unsigned short end, app_info *kek)
-{
-    unsigned short  num = 0;
-    unsigned short  l_end;  
-    unsigned short  j;
-    
-    //kek[0].rid[0] = 'F';
 
     while (i < end)
     {
@@ -100,7 +52,7 @@ unsigned short  fill_app_info(const unsigned char *pRxBuf, unsigned short i, uns
             i += 2;
             while (i < l_end)
             {
-                if (pRxBuf[i] == '9' && pRxBuf[i + 1] == 'F' && pRxBuf[i + 2] == '2' && pRxBuf[i + 3] == 'A')
+                if (!strncmp("9F2A", (pRxBuf + i), 4))
                 {
                     i += 4;
                     i += 2 + char_to_num(pRxBuf[i], pRxBuf[i + 1]) * 2;
@@ -108,18 +60,13 @@ unsigned short  fill_app_info(const unsigned char *pRxBuf, unsigned short i, uns
                 if (pRxBuf[i] == '4' && pRxBuf[i + 1] == 'F')
                 {
                     i += 2;
-                    j = char_to_num(pRxBuf[i], pRxBuf[i + 1]);
-                    i += 2;
-                    if (pRxBuf[i] == 'A' && pRxBuf[i + 1] == '0' && pRxBuf[i + 2] == '0' && pRxBuf[i + 3] == '0' && pRxBuf[i + 4] == '0' && 
-                       pRxBuf[i + 5] == '0' && pRxBuf[i + 6] == '0' && pRxBuf[i + 7] == '6' && pRxBuf[i + 8] == '5' && pRxBuf[i + 9] == '8')
-                        ////////////////
-                    i += j * 2;
+                    i += char_to_num(pRxBuf[i], pRxBuf[i + 1]) * 2 + 2;
+                    ++num;  
                 }
                 if (pRxBuf[i] == '8' && pRxBuf[i + 1] == '7')
                 {
                     i += 2;
-                    j = char_to_num(pRxBuf[i], pRxBuf[i + 1]);
-                    i += j * 2 + 2;
+                    i += char_to_num(pRxBuf[i], pRxBuf[i + 1]) * 2 + 2;
                 }
             }
         }
@@ -129,25 +76,119 @@ unsigned short  fill_app_info(const unsigned char *pRxBuf, unsigned short i, uns
     return num;
 }
 
+void    fill_4f(const unsigned char *pRxBuf, app_info *cur_app, unsigned short pix_mem)
+{
+    cur_app->rid = malloc(sizeof(unsigned char) * 11);
+    strncpy(cur_app->rid, pRxBuf, 10);
+    cur_app->rid[10] = '\0';
+    cur_app->pix = malloc(sizeof(unsigned char) * pix_mem + 1);
+    strncpy(cur_app->pix, pRxBuf + 10, pix_mem);
+    cur_app->pix[pix_mem] = '\0';
+}
+
+void    fill_87(const unsigned char *pRxBuf, app_info *cur_app)
+{
+    if (*(pRxBuf + 1) != '0')
+        cur_app->prior = char_to_num('0', *(pRxBuf + 1));
+}
+
+void  fill_app_info(const unsigned char *pRxBuf, unsigned short i, app_info *apps, unsigned short app_num)
+{
+    unsigned short  num = 0;
+    unsigned short  l_end;  
+    unsigned short  j;
+
+    while (num < app_num)
+    {
+        if (pRxBuf[i] == '6' && pRxBuf[i + 1] == '1')
+        {
+            i += 2;
+            l_end = char_to_num(pRxBuf[i], pRxBuf[i + 1]) * 2 + i + 2;
+            i += 2;
+            while (i < l_end)
+            {
+                if (!strncmp("9F2A", (pRxBuf + i), 4))
+                {
+                    i += 4;
+                    i += 2 + char_to_num(pRxBuf[i], pRxBuf[i + 1]) * 2;
+                }
+                if (pRxBuf[i] == '4' && pRxBuf[i + 1] == 'F')
+                {
+                    i += 2;
+                    j = char_to_num(pRxBuf[i], pRxBuf[i + 1]);
+                    i += 2;
+                    fill_4f(pRxBuf + i, &apps[num], (j - 5) * 2);
+                    i += j * 2;
+                }
+                if (pRxBuf[i] == '8' && pRxBuf[i + 1] == '7')
+                {
+                    i += 4;
+                    fill_87(pRxBuf + i, &apps[num]);
+                    i += 2;
+                }
+            }
+            ++num;
+        }
+        else
+            ++i;
+    }
+}
+
+void    init_apps_prior(app_info *apps, unsigned short app_num)
+{
+    for (unsigned short i = 0; i < app_num; ++i)
+        apps[i].prior = 16;
+}
+
+void    swap_infos(app_info *apps, unsigned short first, unsigned short second)
+{
+    unsigned char   *tmp_str;
+    unsigned short  tmp_num;
+
+    tmp_str = apps[first].pix;
+    apps[first].pix = apps[second].pix;
+    apps[second].pix = tmp_str;
+    tmp_str = apps[first].rid;
+    apps[first].rid = apps[second].rid;
+    apps[second].rid = tmp_str;
+    tmp_num = apps[first].prior;
+    apps[first].prior = apps[second].prior;
+    apps[second].prior = tmp_num;
+}
+
+void    sort_app_prior(app_info *apps, unsigned short app_num)
+{
+    for (unsigned short i = 1; i < app_num; ++i)
+        for (unsigned short j = 0; j < app_num - i; ++j)
+            if (apps[j].prior > apps[j + 1].prior)
+                swap_infos(apps, j, j + 1);
+}
+
+void    print_supported(app_info *apps, unsigned short app_num)
+{
+    for (unsigned short i = 0; i < app_num; ++i)
+        if (!strcmp(apps[i].rid, "A000000658"))
+            printf("%s\n%s %s\n", "RID:       PIX:", apps[i].rid, apps[i].pix);
+}
+
 int main()
 {
     unsigned short  rxBufLen[2] = {510, 122};
-    unsigned char   pRxBuf[122] = "6F39840E325041592E5359532E4444463031A527BF0C2461129F2A038106438701034F07A0000004320001610E8701024F09A000000658101033339000";
-    //unsigned char   pRxBuf[72] = "6F20840E325041592E5359532E4444463031A50EBF0C0B61099F2A038106438701019000";
+    unsigned char   pRxBuf[500] = "6F6C840E325041592E5359532E4444463031A55ABF0C5761094F07A000000658200161134F08A0000006581000039F2A03810643870153610C8701014F07A000000003101061124F07A00000065820028701109F2A0381064361134F08A0000006581000029F2A038106438701529000";
 
-    unsigned short  j;
     unsigned short  i;
     unsigned short  end;
     app_info        *apps;
     unsigned short  pix_m;
     unsigned short  app_num;
     
+    //printf("%d\n", pRxBuf[122]);
     i = 8 + char_to_num(pRxBuf[6], pRxBuf[7]) * 2;
 
     if (pRxBuf[i] != 'A' && pRxBuf[i + 1] != '5')
         return 0;
 
-    /* по идее, теги 'A5', '61', '4F' является одними из обязательных тегов ответа карты на пакет SELECT PPSE, но я не могу пока знать всех нюансов -
+    /* по идее, теги 'A5', '61', '4F' являются одними из обязательных тегов ответа карты на пакет SELECT PPSE, но я не могу пока знать всех нюансов -
     https://www.emvco.com/wp-content/uploads/2017/05/BookB_Entry_Point_Specification_v2_6_20160809023257319.pdf стр. 22, табл. 3-2 */
 
     i += 2 + 2 + 4;
@@ -155,8 +196,11 @@ int main()
     i += 2;
     app_num = app_mem(pRxBuf, i, end);  //количество подходящих приложений
     apps = malloc(sizeof(app_info) * app_num);
-    // printf("%d", sizeof(app_info));
-    // for (j = 0; j < app_num; ++j)
-    fill_app_info(pRxBuf, i, end, apps);
-    printf("%s", apps[0].rid);
+    init_apps_prior(apps, app_num);
+    //printf("%d\n", app_num);
+    fill_app_info(pRxBuf, i, apps, app_num);
+    sort_app_prior(apps, app_num);
+    print_supported(apps, app_num);
+    // for (int j = 0; j < app_num; ++j)
+    //     printf("%s %s %d\n", apps[j].rid, apps[j].pix, apps[j].prior);
 }
